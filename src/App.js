2615f172806a49825aae8343e2795687bfcfc1ba
Anastasia1ukina <lukina02anastasiy@gmail.com>
2023-05-10 01:14:37 +0300

    fix: правит картинки

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React from 'react';
 import Form from './components/form';
 import './App.css';
 import iconDots from './img/dots.svg';
-import iconCalendar from './img/calendar.svg';
-import iconWork from './img/work.svg';
-import iconPencil from './img/pencil.svg';
-import iconNotebook from './img/notebook.svg';
-import iconMessage from './img/message.svg';
+import iconWork from './img/work.png';
+import iconMessage from './img/message.png';
 
 function App() {
   return (
     <div className="App">
       <img className='dots' src={iconDots} alt='dots' width='192' height='245'/>
-      <img className='calendar' src={iconCalendar} alt='calendar' width='2160' height='2160'/>
-      <img className='work' src={iconWork} alt='work' width='192' height='245'/>
-      <img className='notebook' src={iconNotebook} alt='notebook' width='1391' height='1721'/>
-      <img className='pencil' src={iconPencil} alt='pencil' width='1897' height='1017'/>
-      <img className='message' src={iconMessage} alt='message' width='2160' height='2160'/>
+      <img className='work' src={iconWork} alt='work' width='614' height='614'/>
+      <img className='message' src={iconMessage} alt='message' width='620' height='620'/>
       <Form />
     </div>
   );
